96dbba603ff38d96189f98fdcdb55725cb7619b7
Philip Erickson <philiperickson@gmail.com>
2019-12-06 00:17:57 -0700

    fix indent weirdness

--- lib/intcode_program.rb
+++ lib/intcode_program.rb
@@ -52,102 +52,102 @@ class IntcodeProgramError < StandardError
 end
 
 class IntcodeInstruction
-    # op codes
-    ADD = 1
-    MULT = 2
-    HALT = 99
+  # op codes
+  ADD = 1
+  MULT = 2
+  HALT = 99
 
-    def initialize(memory_segment)
-      @op_code = memory_segment[0]
-      @raw_instruction = memory_segment[0...length]
-    end
+  def initialize(memory_segment)
+    @op_code = memory_segment[0]
+    @raw_instruction = memory_segment[0...length]
+  end
 
-    def op_code
-      @op_code
-    end
+  def op_code
+    @op_code
+  end
 
-    def length
-      @length ||= case op_code
-        when *[ADD, MULT]
-          4
-        when HALT
-          1
-        else
-          raise IntcodeProgramError, "Unknown length for op code: #{op_code}"
-      end
-    end
-
-    def inputs
-      @inputs ||= case op_code
-        when *[ADD, MULT]
-          @raw_instruction[1..2]
-        when HALT
-          []
-        else
-          raise IntcodeProgramError, "Unknown inputs for op code: #{op_code}"
-      end
-    end
-
-    def output
-      @output ||= case op_code
-        when *[ADD, MULT]
-          @raw_instruction[3]
-        when HALT
-          nil
-        else
-          raise IntcodeProgramError, "Unknown output for op code: #{op_code}"
-      end
-    end
-
-    def perform!(memory)
-      begin
-        operand_l = memory[inputs[0]]
-        operand_r = memory[inputs[1]]
-        memory[output] = operand_l.send(operator, operand_r)
-      rescue
-        raise IntcodeProgramError, "Invalid operator/operands for instruction: #{self}"
-      end
-    end
-
-    def operator
-      @operator ||= case op_code
-        when ADD
-          :+
-        when MULT
-          :*
-        when HALT
-          nil
-        else
-          raise IntcodeProgramError, "Unknown operator for op code: #{op_code}"
-      end
-    end
-
-    def add?
-      op_code == ADD
-    end
-
-    def mult?
-      op_code == MULT
-    end
-
-    def halt?
-      op_code == HALT
-    end
-
-    def to_s
-      s = case op_code
-        when ADD
-          "ADD"
-        when MULT
-          "MULT"
-        when HALT
-          "HALT"
-        else
-          raise IntcodeProgramError, "Unknown mnemonic for op code: #{op_code}"
-      end
-
-      s += " #{inputs}" if !inputs.empty?
-      s += " -> #{output}" if output
-      s
+  def length
+    @length ||= case op_code
+      when *[ADD, MULT]
+        4
+      when HALT
+        1
+      else
+        raise IntcodeProgramError, "Unknown length for op code: #{op_code}"
     end
   end
+
+  def inputs
+    @inputs ||= case op_code
+      when *[ADD, MULT]
+        @raw_instruction[1..2]
+      when HALT
+        []
+      else
+        raise IntcodeProgramError, "Unknown inputs for op code: #{op_code}"
+    end
+  end
+
+  def output
+    @output ||= case op_code
+      when *[ADD, MULT]
+        @raw_instruction[3]
+      when HALT
+        nil
+      else
+        raise IntcodeProgramError, "Unknown output for op code: #{op_code}"
+    end
+  end
+
+  def perform!(memory)
+    begin
+      operand_l = memory[inputs[0]]
+      operand_r = memory[inputs[1]]
+      memory[output] = operand_l.send(operator, operand_r)
+    rescue
+      raise IntcodeProgramError, "Invalid operator/operands for instruction: #{self}"
+    end
+  end
+
+  def operator
+    @operator ||= case op_code
+      when ADD
+        :+
+      when MULT
+        :*
+      when HALT
+        nil
+      else
+        raise IntcodeProgramError, "Unknown operator for op code: #{op_code}"
+    end
+  end
+
+  def add?
+    op_code == ADD
+  end
+
+  def mult?
+    op_code == MULT
+  end
+
+  def halt?
+    op_code == HALT
+  end
+
+  def to_s
+    s = case op_code
+      when ADD
+        "ADD"
+      when MULT
+        "MULT"
+      when HALT
+        "HALT"
+      else
+        raise IntcodeProgramError, "Unknown mnemonic for op code: #{op_code}"
+    end
+
+    s += " #{inputs}" if !inputs.empty?
+    s += " -> #{output}" if output
+    s
+  end
+end

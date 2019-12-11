3ac61136ddf448694b78ebe5ae3a737e2ff6eac1
Philip Erickson <philiperickson@gmail.com>
2019-12-10 21:29:29 -0700

    remove extra whitespace

--- test/intcode_program_test.rb
+++ test/intcode_program_test.rb
@@ -12,7 +12,7 @@ class IntcodeProgramTest < MiniTest::Test
   def test_equal_to
     # Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
     memory = [3,9,8,9,10,9,4,9,99,-1,8]
-    
+
     input_buffer = [9]
     p = IntcodeProgram.new(memory, input_buffer)
     p.run!
